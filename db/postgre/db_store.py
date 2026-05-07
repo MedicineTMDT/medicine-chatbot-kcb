0c0fee7a923d36ff1296a9b88d76c92c97d11d45
hn-minh <nminh792004@gmail.com>
2026-05-08 00:30:07 +0700

    Lazy Initialization for db

--- db/postgre/db_store.py
+++ db/postgre/db_store.py
@@ -1,32 +1,38 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from .models import Base
-import os
 
-POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
+engine = None
+AsyncSessionLocal = None
 
-engine = create_async_engine(
-    POSTGRES_DATABASE_URL,
-    pool_size=5,
-    max_overflow=0,
-    pool_timeout=30,
-    pool_recycle=1800,
-)
+def _setup():
+    global engine, AsyncSessionLocal
+    if engine is None:
+        POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
+        engine = create_async_engine(
+            POSTGRES_DATABASE_URL,
+            pool_size=5,
+            max_overflow=0,
+            pool_timeout=30,
+            pool_recycle=1800,
+        )
+        AsyncSessionLocal = async_sessionmaker(
+            bind=engine,
+            class_=AsyncSession,
+            autocommit=False,
+            autoflush=False,
+            expire_on_commit=False
+        )
 
 async def init_db():
+    _setup()
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine, 
-    class_=AsyncSession,
-    autocommit=False, 
-    autoflush=False,
-    expire_on_commit=False
-)
-
 async def get_db():
+    _setup()
     db = AsyncSessionLocal()
     try:
         yield db
     finally:
-        await db.close() 
+        await db.close()
